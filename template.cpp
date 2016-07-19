01eed3d20b1424ddc2328f1357effcb05d905ae8
Brayan Henao <bryanhenao96@gmail.com>
2016-07-19 07:19:08 -0500

    Added some code
    Fill array

--- template.cpp
+++ template.cpp
@@ -14,12 +14,14 @@ typedef vector< vi >        vvi;
 const int MOD = int( 1e9+7 );
 const int MAX = int( 2e6 );
 
-#define _ ios_base::sync_with_stdio(false);cin.tie(NULL); //optimizar la lectura y escritura usando cin y cout, no mezclar con stdin ni stdout
+//#define _ ios_base::sync_with_stdio(false);cin.tie(NULL); //optimizar la lectura y escritura usando cin y cout, no mezclar con stdin ni stdout
 #define f(i, a) for(int i = 0; i < a; i++) //
 #define E 2.718281828459045
 #define PI 3.141592653589793
+#define fill ( a , v ) memset ( a , v , sizeof a
 
 int main(){_
+ios_base :: sync_with_stdio ( false ); cin . tie ( NULL
     //esctructura general (La primera linea tine la cant casos de prueba)
     int N;
     cin >> N;
