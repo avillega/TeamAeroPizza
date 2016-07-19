493cc74785e451a5094cac39ad7771a521ad1821
Brayan Henao <bryanhenao96@gmail.com>
2016-07-18 23:05:11 -0500

    Template
    Added c++ basic template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> //todos los headers del standart precompilados
+using namespace std;
+
+typedef long long           ll;
+typedef unsigned long long  ull;
+typedef long double         lf;
+typedef pair< int, int >    pii;
+typedef vector< bool >      vb;
+typedef vector< double >    vd;
+typedef vector< ll >        vll;
+typedef vector< int >       vi;
+typedef vector< vi >        vvi;
+
+const int MOD = int( 1e9+7 );
+const int MAX = int( 2e6 );
+
+#define _ ios_base::sync_with_stdio(false);cin.tie(NULL); //optimizar la lectura y escritura usando cin y cout, no mezclar con stdin ni stdout
+#define f(i, a) for(int i = 0; i < a; i++) //
+#define E 2.718281828459045
+#define PI 3.141592653589793
+
+int main(){_
+    //esctructura general (La primera linea tine la cant casos de prueba)
+    int N;
+    cin >> N;
+    while(N--){
+        //let the magic happens
+    }
+    return 0;
+}
